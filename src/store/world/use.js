2f9c5184c666e21ef7ddae401b29388b252809a2
Shirley89 <yexinxie@163.com>
2020-10-16 16:06:18 +0800

    Update use.js

--- src/store/world/use.js
+++ src/store/world/use.js
@@ -1,4 +1,9 @@
 export default (state) => ({
+  changeCount() {
+    const increment = () => state.count++;
+    const decrement = () => state.count--;
+    return { increment, decrement }
+  },
   increment() {
     state.count++
   },
